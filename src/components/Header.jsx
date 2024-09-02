24a5c5a8a3f9564c682500daa4e81d16e9ffdd67
prohorweb <prohorweb@gmail.com>
2024-09-02 17:34:19 +0300

    Responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import Navigation from './Navigation.jsx';
 
-function Header() {
+const Header = () => {
   return (
-    <header className="flex flex-col sm:flex-row items-center justify-between w-full gap-4 sm:gap-5 mb-8 sm:mb-12">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/2bd43eb46c0be7ca7b8d26e61a11fd47923032810d093e47d61310dae727b3a7?placeholderIfAbsent=true&apiKey=95b40f35762941bfb8c2b598dc225fe8"
-        alt="Company Logo"
-        className="object-contain w-36 sm:w-40 md:w-44 lg:w-48 max-w-full aspect-[4.37]"
-      />
+    <header className="w-full min-h-[813px] max-md:max-w-full">
+      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/42f12883fcb4c64ff0207a5cddce104e77edae5b2471e49ab1809eb9e076d0e9?placeholderIfAbsent=true&apiKey=95b40f35762941bfb8c2b598dc225fe8" alt="" className="-z-10 object-cover absolute inset-0 size-full" />
       <Navigation />
     </header>
   );
-}
+};
 
 export default Header;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,45 +1,77 @@
 import React, { useState } from "react";
+import { Bars3Icon } from '@heroicons/react/24/solid';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 export const Navigation = () => {
-  const [activeLink, setActiveLink] = useState("Focus");
-  const navItems = ["Focus", "Services", "Clients", "Team", "Contact"];
-  const navItem = ["Focus", "Services", "Clients", "Team", "Contact"];
+  const [activeLink, setActiveLink] = useState(null);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const navItems = [
+    { name: 'О соборе', url: 'about' },
+    { name: 'Новости', url: 'news' },
+    { name: 'Деятельность', url: 'services' },
+    { name: 'Главная', url: '/' },
+    { name: 'Образование', url: 'edu' },
+    { name: 'Прихожанам', url: 'crowd' },
+    { name: 'Контакты', url: 'contact' },
+  ];
 
 
   return (
-    <nav className="flex flex-col items-center sm:items-end">
-      <ul className="flex flex-wrap justify-center sm:justify-end">
-
-        {/* {navItems.map((item) => (
-            <li
-              key={item}
-              className={`inline-block mr-4 cursor-pointer ${
-                activeLink === item ? "text-orange-200" : ""
-              }`}
-              onClick={() => setActiveLink(item)}
-            >
-              {item}
+    <>
+      <nav className="px-20 pt-10 pb-16 max-md:px-5 max-sm:hidden">
+        <ul className="flex flex-wrap justify-center lg:grid lg:grid-cols-7 lg:gap-7 gap-5">
+          {navItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <li className={`flex flex-wrap  content-center justify-center ${item.name === 'Главная' ? 'max-lg:hidden' : ''}`} >
+                <a key={index}
+                  href={`#${item.url.toLowerCase()}`}
+                  className={` xl:text-base text-xs tracking-[.035em] text-white font-['Georgia'] uppercase hover:text-orange-200 ${activeLink === item ? "text-orange-200" : ""
+                    }`}
+                  onClick={() => setActiveLink(item)}
+                >
+                  {item.name === 'Главная' ? (
+                    <>
+                      <img loading="lazy" src="/logo.svg" alt="" className="" />
+                    </>
+                  ) : item.name}
+                </a>
               </li>
-          ))} */}
-        {navItem.map((item, index) => (
-          <li
-            className={`inline-block mr-4 cursor-pointer `}
-          >
+            </React.Fragment>
+          ))}
+        </ul>
 
-            <a key={index}
-              href={`#${item.toLowerCase()}`}
-              className={`text-white hover:text-orange-200 transition-colors duration-300 ${activeLink === item ? "text-orange-200 border-b-2  border-orange-200 border-solid w-fill pb-2" : ""
-                }`}
-              onClick={() => setActiveLink(item)}
-            >
+      </nav>
+        <nav className="fixed w-full text-indigo-200 hidden max-sm:block ">
+          <div className="flex flex-wrap justify-between content-center text-sm/6 bg-blue-950 p-2 uppercase font-['Georgia'] tracking-[.035em]">
+              Николо-Морской собор
+              <button onClick={() => setMobileMenu(!mobileMenu)}>
+              {mobileMenu ? <XMarkIcon className=" w-6 h-6" /> : <Bars3Icon className="w-6 h-6" />}
+            </button>
+          </div>
+        {/* <div className={`fixed top-0 right-0 ${mobileMenu ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out`}>Menu Item 1</div> */}
+        <ul className={`fixed top-10 right-0 w-1/2 bg-blue-950 px-4 ${mobileMenu ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out`}>
+        {navItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <li className={`pb-3`} >
+                <a key={index}
+                  href={`#${item.url.toLowerCase()}`}
+                  className={`tracking-[.035em] hover:text-orange-200 ${activeLink === item ? "text-orange-200" : ""
+                    }`}
+                  onClick={() => setActiveLink(item)}
+                >
+                  {item.name}
+                </a>
+              </li>
+            </React.Fragment>
+          ))}
+        </ul>
+        </nav>
 
-              {item}
-            </a>
-            {/* <div className="shrink-0 mt-2 h-px border border-orange-200 border-solid w-fill" /> */}
-          </li>
-        ))}
-      </ul>
-    </nav>
+
+
+    </>
+
   );
 }
 
